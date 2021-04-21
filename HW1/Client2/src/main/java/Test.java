3fb1046dc7d250bcccc0c04fe1320740eb71a378
马天择 <matianze@s-MacBook-Pro.local>
2021-04-20 23:12:34 -0700

    add photo

--- HW1/Client2/src/main/java/Test.java
+++ HW1/Client2/src/main/java/Test.java
@@ -8,19 +8,21 @@ public class Test {
 
   public static void main(String[] args) throws IOException {
     HttpClient client = new HttpClient();
-//    String url = "http://ec2-3-90-248-99.compute-1.amazonaws.com:8080/items/store/1";
-    String url = "http://localhost:8888/items/store/1";
+    String url = "http://ec2-3-90-248-99.compute-1.amazonaws.com:8080/items/store/1";
+//    String url = "http://localhost:8888/items/store/1";
     GetMethod method = new GetMethod(url);
     client.executeMethod(method);
     String resp = convertStreamToString(method.getResponseBodyAsStream());
+    System.out.println("The top 10 items for store 1:");
     System.out.println(resp);
 
     HttpClient client1 = new HttpClient();
-//    String url1 = "http://ec2-3-90-248-99.compute-1.amazonaws.com:8080/items/top10/69501";
-    String url1 = "http://localhost:8888/items/top10/69501";
+    String url1 = "http://ec2-3-90-248-99.compute-1.amazonaws.com:8080/items/top10/69501";
+//    String url1 = "http://localhost:8888/items/top10/69501";
     GetMethod method1 = new GetMethod(url1);
     client1.executeMethod(method1);
     String resp1 = convertStreamToString(method1.getResponseBodyAsStream());
+    System.out.println("The top store for item 69501:");
     System.out.println(resp1);
   }
 
